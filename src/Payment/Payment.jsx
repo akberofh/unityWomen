0d61ef461243ecbc569c367be9baa12fc406e378
akberofh <sd85bejal@code.edu.az>
2025-03-15 21:44:07 +0400

    sfgfjug

--- src/Payment/Payment.jsx
+++ src/Payment/Payment.jsx
@@ -4,8 +4,8 @@ const Payment = () => {
   const [step, setStep] = useState(1);
   
   return (
-    <div className="flex justify-between border max-w-7xl dark:to-black mx-auto p-5 gap-5">
-      <div className="flex-1 p-5 rounded-lg  dark:to-black shadow-md">
+    <div className="flex justify-between border max-w-7xl dark:to-black mx-auto p-5 gap-5 flex-wrap">
+      <div className="flex-1 p-5 rounded-lg dark:to-black shadow-md w-full sm:w-2/3 lg:w-2/3">
         <div className="flex justify-between mb-5">
           <div className={`flex-1 text-center py-2 border border-[#b87333] rounded-md font-bold ${step === 1 ? 'bg-[#b87333] text-white' : 'text-[#b87333]'}`}>1. Faktura ünvanı</div>
           <div className={`flex-1 text-center py-2 border border-[#b87333] rounded-md font-bold ${step === 2 ? 'bg-[#b87333] text-white' : 'text-[#b87333]'}`}>2. Çatdırılma ünvanı</div>
@@ -14,28 +14,28 @@ const Payment = () => {
         
         {step === 1 && (
           <div>
-            <h2 className="text-xl  dark:to-black font-semibold mb-4">Faktura ünvanı</h2>
+            <h2 className="text-xl dark:to-black font-semibold mb-4">Faktura ünvanı</h2>
             <form>
-            <label className="block text-sm  font-semibold mt-4">Ad</label>
-          <input type="text" className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Ad</label>
+              <input type="text" className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md" required />
 
-          <label className="block text-sm  font-semibold mt-4">Soyad</label>
-          <input type="text" className="w-full p-2 mt-2 dark:text-black  border dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Soyad</label>
+              <input type="text" className="w-full p-2 mt-2 dark:text-black border dark:to-black rounded-md" required />
 
-          <label className="block text-sm font-semibold mt-4">Email ünvanı</label>
-          <input type="email" className="w-full p-2 mt-2 dark:text-black  border dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Email ünvanı</label>
+              <input type="email" className="w-full p-2 mt-2 dark:text-black border dark:to-black rounded-md" required />
 
-          <label className="block text-sm font-semibold mt-4">Əlaqə nömrəsi</label>
-          <input type="tel" className="w-full p-2 mt-2 dark:text-black  border dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Əlaqə nömrəsi</label>
+              <input type="tel" className="w-full p-2 mt-2 dark:text-black border dark:to-black rounded-md" required />
 
-          <label className="block text-sm font-semibold mt-4">Ünvan</label>
-          <input type="text" className="w-full p-2 mt-2 dark:text-black  border dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Ünvan</label>
+              <input type="text" className="w-full p-2 mt-2 dark:text-black border dark:to-black rounded-md" required />
 
-          <label className="block text-sm font-semibold mt-4">Poçt indeksi</label>
-          <input type="text" className="w-full p-2 mt-2 dark:text-black  border dark:to-black rounded-md" />
+              <label className="block text-sm font-semibold mt-4">Poçt indeksi</label>
+              <input type="text" className="w-full p-2 mt-2 dark:text-black border dark:to-black rounded-md" />
 
-          <label className="block text-sm font-semibold mt-4">Şəhər</label>
-              <input type="email" className="w-full p-2 dark:text-black  mt-2 border dark:to-black rounded-md" required />
+              <label className="block text-sm font-semibold mt-4">Şəhər</label>
+              <input type="email" className="w-full p-2 dark:text-black mt-2 border dark:to-black rounded-md" required />
               
               <button type="button" onClick={() => setStep(2)} className="bg-[#b87333] text-white py-2 px-4 rounded-md hover:bg-[#a0652e] mt-6 float-right">Davam et →</button>
             </form>
@@ -47,7 +47,7 @@ const Payment = () => {
             <h2 className="text-xl font-semibold mb-4">Çatdırılma ünvanı</h2>
             <form>
               <label className="block text-sm font-semibold mt-4">Şəhər</label>
-              <select className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md">
+              <select className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md">
                 <option>Bakı</option>
                 <option>Gəncə</option>
                 <option>Sumqayıt</option>
@@ -55,7 +55,7 @@ const Payment = () => {
               </select>
               
               <label className="block text-sm font-semibold mt-4">Çatdırılma növü</label>
-              <select className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md">
+              <select className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md">
                 <option>Kuryer</option>
                 <option>Poçt</option>
                 <option>Mağazadan götürmə</option>
@@ -74,16 +74,16 @@ const Payment = () => {
             <h2 className="text-xl font-semibold mb-4">Ödəniş</h2>
             <form>
               <label className="block text-sm font-semibold mt-4">Kart Nömrəsi</label>
-              <input type="text" className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md" placeholder="**** **** **** ****" required />
+              <input type="text" className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md" placeholder="**** **** **** ****" required />
               
               <label className="block text-sm font-semibold mt-4">Son istifadə tarixi</label>
-              <input type="text" className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md" placeholder="MM/YY" required />
+              <input type="text" className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md" placeholder="MM/YY" required />
               
               <label className="block text-sm font-semibold mt-4">CVV</label>
-              <input type="text" className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md" placeholder="***" required />
+              <input type="text" className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md" placeholder="***" required />
               
               <label className="block text-sm font-semibold mt-4">Şəkil yüklə</label>
-              <input type="file" className="w-full p-2 mt-2 border dark:text-black  dark:to-black rounded-md" />
+              <input type="file" className="w-full p-2 mt-2 border dark:text-black dark:to-black rounded-md" />
               
               <div className="flex justify-between mt-6">
                 <button type="button" onClick={() => setStep(2)} className="bg-gray-400 text-white py-2 px-4 rounded-md">← Geri</button>
@@ -94,7 +94,7 @@ const Payment = () => {
         )}
       </div>
       
-      <div className="w-80 border p-5 rounded-lg shadow-md">
+      <div className="w-full sm:w-1/3 lg:w-1/3 border p-5 rounded-lg shadow-md mt-5 sm:mt-0">
         <h3 className="text-xl font-semibold">Sifariş özeti</h3>
         {/* Burada sifariş özeti əlavə ediləcək */}
       </div>
