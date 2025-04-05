eb4cab72372684ff161b7f34d94402005d17959c
akberofh <sd85bejal@code.edu.az>
2025-04-05 20:49:59 +0400

    dfghsfdrdddj

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -155,7 +155,7 @@ const Register = () => {
                             <input
                                 type="text"
                                 name="referralCode"
-                                placeholder="Liderin kodunu daxil edin"
+                                placeholder="Liderin kodunu daxil edin!"
                                 onChange={(e) => setReferralCode(e.target.value)}
                                 required={referralRequired}
                                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-400 focus:border-blue-400 text-md"
